dc6be00330d4041cb24c9e3019b8a19551c3e629
paumaneja <paumaneja@gmail.com>
2024-12-15 23:25:55 +0100

    first commit

---
+++ VirtualPet/src/main/java/cat/itacademy/s05/t02/n01/VirtualPetApplication.java
@@ -0,0 +1,14 @@
+package cat.itacademy.s05.t02.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VirtualPetApplication.class, args);
+	}
+
+}
---
+++ VirtualPet/src/main/java/cat/itacademy/s05/t02/n01/model/User.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s05.t02.n01.model;
+
+public class User {
+}
