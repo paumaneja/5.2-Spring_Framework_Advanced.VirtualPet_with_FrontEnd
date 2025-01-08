79842d74e2bbf723337a6d56e7f80854dbd4a8c6
paumaneja <paumaneja@gmail.com>
2025-01-08 11:49:54 +0100

    modified

--- VirtualPet/src/main/java/cat/itacademy/s05/t02/n01/auth/AuthService.java
+++ VirtualPet/src/main/java/cat/itacademy/s05/t02/n01/auth/AuthService.java
@@ -3,11 +3,14 @@ package cat.itacademy.s05.t02.n01.auth;
 import cat.itacademy.s05.t02.n01.enums.Role;
 import cat.itacademy.s05.t02.n01.jwt.JwtService;
 import cat.itacademy.s05.t02.n01.model.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import cat.itacademy.s05.t02.n01.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +19,15 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
